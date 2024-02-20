26c3a00559f719d55309a01a403ef65388e460f2
Javier <jh19059918@gmail.com>
2024-02-20 13:06:32 -0600

    🌟Feat: Author Name Details : Create the Author Name component

---
+++ src/components/AuthorName.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="div">
+        <h1>Author Name: </h1>
+        <p> 
+            <a target="_blank " href="https://github.com/Javierhr1234">
+                Heredia Rodriguez Javier</a> 
+        </p>
+    </div>
+</template>
